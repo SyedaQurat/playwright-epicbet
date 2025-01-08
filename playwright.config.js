b506957379486d6cb23674b39fdf6d7a7a84c0bf
Syeda <syeda.annie.asad@gmail.com>
2025-01-08 21:54:24 +0200

    set headless: true

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     ],
     use: {
         browserName: 'chromium',
-        headless: false,
+        headless: true,
         viewport: { width: 1280, height: 720 },
         screenshot: 'only-on-failure',
         baseURL: 'https://epicbet.com/en/sports/'
